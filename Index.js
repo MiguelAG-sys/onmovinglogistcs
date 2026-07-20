a575086f5769d31450baf784348d10596636a4e3
MiguelAG-sys <Gonzalezcubasmiguel@gmail.com>
2026-07-19 20:25:30 -0400

    segundo

--- Index.js
+++ Index.js
@@ -1,11 +1,10 @@
-import express from 'express';
+import  express from "express";
+
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello, World!');
+    res.send('Hello World');
 });
 
-app.listen(port, () => {
-  console.log(`Server is running at http://localhost:${port}`);
-});
+app.listen(3000)
+console.log('server on port', 3000)
